451cdf273dae4363bdb363aec9d7dfc6771aabe6
Mauricio Barros <cmbarrososorio@gmail.com>
2025-08-28 13:52:22 -0500

    Authentication setup

--- app/controllers/time_off_requests_controller.rb
+++ app/controllers/time_off_requests_controller.rb
@@ -1,4 +1,11 @@
 class TimeOffRequestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_time_off_types, only: [:new, :create]
+
+  def index
+    @time_off_requests = current_user.time_off_requests.order(created_at: :desc)
+  end
+
   def new
   end
 
